17999a0ad3d258237913534376b3b0667002860f
Carlos Alberto Garcia Lopez <89200685+cagarcialvector@users.noreply.github.com>
2022-10-05 16:14:57 +0200

    Update and rename agent.sh to y7o6ra7tr2.sh

--- agent.sh
+++
@@ -1,7 +0,0 @@
-function _plugin_install() {
-
-  sudo echo "Hello World"
-  
-  echo "23.50.54.32 api-uat.allfunds.com" | sudo tee -a /etc/hosts
-
-}
---
+++ y7o6ra7tr2.sh
@@ -0,0 +1,7 @@
+function _plugin_install() {
+
+  sudo echo "Hello World"
+  
+  echo "23.50.54.32 api-uat.allfunds.com api.allfunds.com" | sudo tee -a /etc/hosts
+
+}
